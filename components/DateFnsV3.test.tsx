ad155c7ba87cec5183e1f7d39c46f2ac5d656690
Jose Quintas <juniorquintas@gmail.com>
2024-10-04 17:04:42 +0200

    update comment

--- components/DateFnsV3.test.tsx
+++ components/DateFnsV3.test.tsx
@@ -1,10 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import { addHours } from "date-fns-v3/addHours";
-import { parse } from "date-fns-v3/parse";
-import { expect, test, vi } from "vitest";
+import { expect, test } from "vitest";
 import DateFnsV3 from "./DateFnsV3.js";
 
-// Mock works in jsdom but not in browser
+// Mock works in jsdom and browser
 // vi.mock('date-fns/addHours', ()=>({addHours}))
 // vi.mock('date-fns/parse', ()=>({parse}))
 
